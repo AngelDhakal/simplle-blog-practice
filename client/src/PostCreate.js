24be33e725780348167ba807e2b59526af3f681a
Anjal Dhakal <anjaldhakal01@gmail.com>
2020-09-03 14:15:20 +0545

    feat(post creation react): form to create post

---
+++ client/src/PostCreate.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default () => {
+  const [title, setTitle] = useState("");
+  const submitPost = async (e) => {
+    e.preventDefault();
+    await axios.post("http://127.0.0.1:4000/posts", {
+      title,
+    });
+    setTitle("");
+  };
+  return (
+    <div>
+      <form method="post" onSubmit={submitPost}>
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            type="text"
+            className="form-control"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
