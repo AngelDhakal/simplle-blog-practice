c6629f870c539edee7a2f1799c9dd7c9daad26cd
Anjal Dhakal <anjaldhakal01@gmail.com>
2020-09-03 14:13:59 +0545

    feat(comments endpoints): comments creation and retrieval endpoints created

---
+++ comments/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const {
+    randomBytes
+} = require("crypto");
+
+app.use(express.json());
+
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+    res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+    const commentId = randomBytes(4).toString("hex");
+    const {
+        content
+    } = req.body;
+
+    const comments = commentsByPostId[req.params.id] || [];
+
+    comments.push({
+        id: commentId,
+        content
+    });
+
+    commentsByPostId[req.params.id] = comments;
+
+    res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+    console.log("listening at port 4001");
+});
